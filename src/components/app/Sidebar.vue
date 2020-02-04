d5f3c2b45337ee796a39bb635f1cc8f4b065ed72
sanya-shch <shchipkov.sanya@ukr.net>
2020-02-04 19:38:06 +0200

    added pages and layouts

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <li>
+      <a href="#" class="waves-effect waves-grey pointer">Счет</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-grey pointer">История</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-grey pointer">Планирование</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-grey pointer">Новая запись</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-grey pointer">Категории</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "Sidebar"
+  }
+</script>
