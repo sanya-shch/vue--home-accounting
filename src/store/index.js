af111293f038f347748cc33d59311ba96d27cebb
sanya-shch <shchipkov.sanya@ukr.net>
2020-02-06 01:04:08 +0200

    edited home page

--- src/store/index.js
+++ src/store/index.js
@@ -21,6 +21,12 @@ export default new Vuex.Store({
     error: s => s.error
   },
   actions: {
+    async fetchCurrency() {
+      const key = process.env.VUE_APP_FIXER
+
+      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB,UAH`)
+      return await res.json()
+    }
   },
   modules: {
     auth,
