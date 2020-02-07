858c25dcf25a990982a5d43b45c54206278bf535
sanya-shch <shchipkov.sanya@ukr.net>
2020-02-07 03:14:10 +0200

    added route protection

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -25,6 +25,7 @@
 <script>
   import Navbar from '@/components/app/Navbar'
   import Sidebar from "../components/app/Sidebar";
+  import messages from "../utils/messages";
 
   export default {
     name: 'main-layout',
@@ -32,6 +33,16 @@
       isOpen: true,
       loading: true
     }),
+    computed: {
+      error() {
+        return this.$store.getters.error
+      }
+    },
+    watch: {
+      error(firestoreError) {
+        this.$error(messages[firestoreError.code] || 'Что-то пошло не так')
+      }
+    },
     async mounted() {
       if (!Object.keys(this.$store.getters.info).length) {
         await this.$store.dispatch('fetchInfo')
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,4 +1,5 @@
 export default {
+  login: 'Для начала войдите в систему',
   logout: 'Вы вышли из системы',
   "auth/user-not-found": 'Что то пошло не так',
   "auth/wrong-password": 'Что то пошло не так',
