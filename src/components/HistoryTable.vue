76a044548283c8ba815f16a3fd0047546301c86b
sanya-shch <shchipkov.sanya@ukr.net>
2020-02-07 15:19:50 +0200

    edited history page

---
+++ src/components/HistoryTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <table>
+    <thead>
+    <tr>
+      <th>#</th>
+      <th>Сумма</th>
+      <th>Дата</th>
+      <th>Категория</th>
+      <th>Тип</th>
+      <th>Открыть</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <tr v-for="(rec, id) of records" :key="rec.id">
+      <td>{{id + 1}}</td>
+      <td>{{rec.amount | currency('UAH')}}</td>
+      <td>{{rec.date | date('datetime')}}</td>
+      <td>{{rec.categoryName}}</td>
+      <td>
+        <span class="white-text badge" :class="[rec.typeClass]">{{rec.typeText}}</span>
+      </td>
+      <td>
+        <button
+            class="btn-small btn"
+            @click="$router.push('/detail/' + rec.id)"
+            v-tooltip="'Посмотреть запись'"
+        >
+          <i class="material-icons">open_in_new</i>
+        </button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: "HistoryTable",
+    props: {
+      records: {
+        required: true,
+        type: Array
+      }
+    }
+  }
+</script>
--- src/views/History.vue
+++ src/views/History.vue
@@ -8,36 +8,40 @@
       <canvas></canvas>
     </div>
 
-    <section>
-      <table>
-        <thead>
-        <tr>
-          <th>#</th>
-          <th>Сумма</th>
-          <th>Дата</th>
-          <th>Категория</th>
-          <th>Тип</th>
-          <th>Открыть</th>
-        </tr>
-        </thead>
+    <Loader v-if="loading"/>
 
-        <tbody>
-        <tr>
-          <td>1</td>
-          <td>1212</td>
-          <td>12.12.32</td>
-          <td>name</td>
-          <td>
-            <span class="white-text badge red">Расход</span>
-          </td>
-          <td>
-            <button class="btn-small btn">
-              <i class="material-icons">open_in_new</i>
-            </button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+    <p class="center" v-else-if="!records.length">Записей пока нет. <router-link to="/record">Добавить новую запись</router-link></p>
+
+    <section v-else>
+      <HistoryTable :records="records"/>
     </section>
   </div>
 </template>
+
+<script>
+  import HistoryTable from "../components/HistoryTable";
+
+  export default {
+    data: () => ({
+      categories: [],
+      records: [],
+      loading: true
+    }),
+    async mounted() {
+      const records = await this.$store.dispatch('fetchRecords')
+      this.categories = await this.$store.dispatch('fetchCategories')
+
+      this.records = records.map(r => {
+        return {
+          ...r,
+          categoryName: this.categories.find(c => c.id === r.categoryId).title,
+          typeText: r.type === 'income' ? 'Доход' : 'Расход',
+          typeClass: r.type === 'income' ? 'green' : 'red'
+        }
+      })
+
+      this.loading = false
+    },
+    components: {HistoryTable}
+  }
+</script>
